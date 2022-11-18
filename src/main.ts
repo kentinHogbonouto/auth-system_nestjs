6de331b691a3f17d7c8714a3302f25c5234425b4
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-11-18 21:28:25 +0100

    remove env file

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { environments } from './environments/environments';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import helmet from 'helmet';
 import * as compression from 'compression';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -13,6 +18,17 @@ async function bootstrap() {
   app.enableShutdownHooks;
   app.use(helmet());
   app.use(compression());
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const config = new DocumentBuilder()
+    .setTitle('Authentication system Server')
+    .setDescription('Showcare Sales server API Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('/docs', app, document);
 
   const logger = new Logger('NestAuthServer');
 
